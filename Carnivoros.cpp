2c6649bb7e11f342f6eb5709b5e871db6a71df08
fabiohenriquez <fa.henriquez@hotmail.com>
2018-06-08 12:20:20 -0600

    herc

--- Carnivoros.cpp
+++ Carnivoros.cpp
@@ -4,24 +4,10 @@
 #include "Carnivoros.h"
 using namespace std;
 
-<<<<<<< HEAD
 Carnivoros::Carnivoros(string nombre, int altura, int peso, string fechaCreacion, string sexo, int longitud,string organo,string tipo):
 Dinosaurio(nombre,altura,peso,fechaCreacion,sexo,longitud){
     this->organo = organo;
     this->coc = tipo;
-=======
-Carnivoros::Carnivoros(string pOrgano, 
-string pCoc):Dinosaurio(string pNombre, int pAltura, double pPeso, string pFecha, 
-string pSexo, double pLongitud){
-    organo = pOrgano;
-    coc = pCoc;
-    this->pNombre=pNombre;
-    this->pAltura=pAltura;
-    this->pPeso=pPeso;
-    this->pFecha=pFecha;
-    this->pSexo=pSexo;
-    this->pLongitud=pLongitud;
->>>>>>> 3d652dc47938d532e56a8eadfbdbd5e7cf9fe5ee
 }
 
 string Carnivoros::getOrgano(){
--- Tyranosaurio.cpp
+++ Tyranosaurio.cpp
@@ -6,19 +6,9 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 Tyranosaurio::Tyranosaurio(string nombre, int altura, int peso, string fechaCreacion, string sexo, int longitud,string organo,string tipo,int consumoDiario,int numColmillos):Carnivoros(nombre,altura,peso,fechaCreacion,sexo,longitud,organo,tipo){
     this-> consumo = consumoDiario;
     this-> colmillos = numColmillos;
-=======
-Tyranosaurio::Tyranosaurio(string pNombre, int pAltura, double pPeso, string pFecha, string pSexo, double pLongitud, string pOrgano, 
-string pCoc, int pColmillos, 
-int pConsumo):Carnivoros(pNombre,pAltura, pPeso, pFecha, pSexo,pLongitud, pOrgano, 
- pCoc, pColmillos, 
-pConsumo){
-    colmillos = pColmillos;
-    consumo = pConsumo;
->>>>>>> 3d652dc47938d532e56a8eadfbdbd5e7cf9fe5ee
 }
 
 int Tyranosaurio::getColmillos(){
